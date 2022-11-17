8ff8e76a7e35c81eeb4bda466a9b02b6f910cd02
Daniel Volchek <danielvolchek123@gmail.com>
2022-11-16 23:55:01 -0500

    test

--- lua/telescope/_extensions/tailiscope.lua
+++ lua/telescope/_extensions/tailiscope.lua
@@ -1,8 +1,10 @@
 return require("telescope").register_extension({
 	setup = function(ext_config, config)
 		-- access extension config and user config
+		vim.notify("ext_config is ")
+		vim.notify(vim.inspect(ext_config))
 	end,
 	exports = {
-		tailiscope = require("tailiscope"),
+		tailiscope = require("tailiscope")(),
 	},
 })
