b898390338d1d25b87ce22646f40bc63180be88d
Daniel Volchek <danielvolchek123@gmail.com>
2022-11-21 23:26:34 -0500

    added opts

--- lua/telescope/_extensions/tailiscope.lua
+++ lua/telescope/_extensions/tailiscope.lua
@@ -1,11 +1,23 @@
+local opts = {}
+
 return require("telescope").register_extension({
 	setup = function(ext_config, config)
 		-- access extension config and user config
-		_G.tailiscope_config = ext_config
+		require("tailiscope").config = vim.tbl_deep_extend("force", M.config, ext_config)
+		opts = config
 	end,
 	exports = {
-		tailiscope = function()
-			require("tailiscope")("base")
+		base = function()
+			require("tailiscope").picker("base", opts)
+		end,
+		categories = function()
+			require("tailiscope").picker("categories", opts)
+		end,
+		classes = function()
+			require("tailiscope").picker("classes", opts)
+		end,
+		all = function()
+			require("tailiscope").picker("all", opts)
 		end,
 	},
 })
